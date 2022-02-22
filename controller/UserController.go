95b557b80fa262614d87b2b8c363af77035a92d8
Lin Xin <widayn@prmlk.com>
2022-02-22 09:26:36 +0800

    添加用户登录功能

--- controller/UserController.go
+++ controller/UserController.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"prmlk.com/nextdebug/common"
@@ -13,8 +15,9 @@ func Register(ctx *gin.Context) {
 	name := ctx.PostForm("name")
 	password := ctx.PostForm("password")
 
-	if len(name) == 0 {
-		name = "Undefined"
+	if len(name) < 3 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户名不能小于三位"})
+		return
 	}
 
 	if len(password) < 6 {
@@ -22,9 +25,18 @@ func Register(ctx *gin.Context) {
 		return
 	}
 
+	if isNameExist(DB, name) {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户名已经被注册"})
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"code": 500, "msg": "加密错误"})
+	}
 	newUser := model.User{
 		Name:     name,
-		Password: password,
+		Password: string(hashedPassword),
 	}
 	tx := DB.Create(&newUser)
 	if tx.Error != nil {
@@ -33,4 +45,40 @@ func Register(ctx *gin.Context) {
 		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 200, "msg": "注册成功"})
 	}
 	log.Println(name, password)
+}
+
+func Login(ctx *gin.Context) {
+	DB := common.GetDB()
+	name := ctx.PostForm("name")
+	password := ctx.PostForm("password")
+
+	var user model.User
+
+	DB.Where("name = ?", name).First(&user)
+
+	if user.ID <= 0 {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": 400, "msg": "用户名或密码错误"})
+		return
+	}
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err == nil {
+		ctx.JSON(http.StatusAccepted, gin.H{
+			"code": 200,
+			"data": gin.H{"token": "123"},
+			"msg":  "登陆成功"})
+		return
+	} else {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": "400", "msg": "用户名或密码错误"})
+		return
+	}
+}
+
+func isNameExist(db *gorm.DB, name string) bool {
+	var user model.User
+	db.Where("name = ?", name).First(&user)
+	if user.ID > 0 {
+		return true
+	} else {
+		return false
+	}
 }
